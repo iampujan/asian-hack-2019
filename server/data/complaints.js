56020d543f5a302de7ab00fcc27c1acdd44beab1
YednapNibas <pandeyfcb7849@gmail.com>
2019-08-24 23:19:55 +0545

    schema modified

--- server/data/complaints.js
+++ server/data/complaints.js
@@ -8,28 +8,31 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   let complaintsSchema = new mongoose.Schema({
     body: String,
-    uid: String,
-    complaint: [{ body: String, uid: String }],
-    upVoteId: Array
+    deviceId: String,
+    upVoteId: String
   });
+
+  //   let comp = "too many factories",
+  //     id = "AJFSDLKFJDSFLKJ3324",
+  //     voteId = "JDLFJJ4984359874987";
 
   let Complaints = new mongoose.model("Complaints", complaintsSchema);
 
   let temp = new Complaints({
-    body: "Pollution due to Bhaktapur",
-    uid: "ADLFJQ948U5324328",
-    upVotes: [{ upVoteId: "lsjfjsdlfjSDFSDLKFJ", upVoteId: "ASLFJSDLFKJSDF" }]
+    body: comp,
+    deviceId: id,
+    upVoteId: voteId
   });
 
   temp.save((err, temp) => {
-    if (err) {
-      return console.error(err);
-    }
+    if (err) return console.error(err);
   });
 
-  Complaints.find({ uid: /^ADLFJQ948U5324328/ }, (err, sfd) => {
+  Complaints.find({ deviceId: /^AJFSDLKFJDSFLKJ3324/ }, (err, sfd) => {
     if (err) console.error("err");
-    console.log(sfd);
+    sfd.forEach(element => {
+      console.log(element.upVoteId);
+    });
   });
 
   complaintsSchema.methods.speak = () => {};
